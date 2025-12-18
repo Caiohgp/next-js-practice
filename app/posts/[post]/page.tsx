3db00e0973ddbbceed6bfbbcd28c5b73729fb77d
Caiohgp <caio.102008@gmail.com>
2025-12-18 00:56:51 -0300

    Adding Markdown files

--- app/posts/[post]/page.tsx
+++ app/posts/[post]/page.tsx
@@ -1,4 +1,7 @@
+import { MDXRemote } from "next-mdx-remote/rsc"
 import { notFound } from "next/navigation"
+import path from "path"
+import fs, { readFileSync } from 'fs'
 
 type PostsPageProps = {
   params: {
@@ -16,8 +19,16 @@ export default async function PostPage({ params }: PostsPageProps) {
 
   const blogPost = await response.json()
 
+  const file = fs.readFileSync(
+    path.join('content',`${postId}.mdx`)
+  )
+
   return (
     <>
+      <article className="prose dark:prose-invert">
+        <MDXRemote source={file} />
+      </article>
+      
       <h2 className="text-4xl mb-5">{blogPost.title}</h2>
       <p>{blogPost.text}</p>
       <span>{blogPost.views}</span>
