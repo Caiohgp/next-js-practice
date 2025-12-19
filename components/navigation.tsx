9353f80b9b980ffdacac991cf27d0ed0a98b00aa
Caiohgp <caio.102008@gmail.com>
2025-12-18 23:50:30 -0300

    Adding posts to the menu

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,10 +5,10 @@ export default function Navigation(){
     return (
         <nav className="font-mono">
             <div className="flex space-x-4 flex-col md:flex-row">
-                <Link href="/" className={style.menu}>Home</Link>
                 <Link href="/about" className={style.menu}>About</Link>
                 <Link href="/gallery" className={style.menu}>Gallery</Link>
                 <Link href="/about/projects" className={style.menu}>Projects</Link>
+                <Link href="/posts" className={style.menu}>Posts</Link>
                 <Link href="/posts/1" className={style.menu}>Go to post 1</Link>
             </div>
         </nav>
